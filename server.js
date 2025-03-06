c1096c080f4c8a6fd1081e3afdcd9eb23f58ad06
DUA_Designs <dua99mech.engr@gmail.com>
2025-03-06 15:06:40 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+// const { spokeitDigitalSignature } = require('./src/documentmerge/merge-document-to-pdf');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+// const { uploadPdfToGCS } = require('./google-cloud/storage');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('This server is for spokeit.');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// app.post('/document-generation', async (req, res) => {
+//   const { name, signature, date } = req.body;
+
+//   if (!name || !date) {
+//     return res.status(400).send('Please provide all required fields');
+//   }
+
+//   try {
+//     const pdfBuffer = await spokeitDigitalSignature({
+//       user: {
+//         Name: name,
+//         Date: date,
+//       },
+//       photograph: signature,
+//     });
+
+//     const fileName = `${name}-${Date.now()}.pdf`;
+//     const gcsUrl = await uploadPdfToGCS(pdfBuffer, fileName);
+
+//     res.send(`Document generated and uploaded successfully to: ${gcsUrl}`);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Error generating or uploading document');
+//   }
+// });
+
+app.use((req, res, next) => {
+  res.status(404).send('Not Found!');
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
